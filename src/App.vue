7022582f5c6582af9fc0c5263f78b63c4214cd7a
0crash0 <0crash0@users.noreply.github.com>
2024-10-03 14:45:26 +0300

    [cicd]

--- src/App.vue
+++ src/App.vue
@@ -8,7 +8,7 @@ import TheWelcome from './components/TheWelcome.vue'
     <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
-      <HelloWorld msg="You jst did it!" />
+      <HelloWorld msg="You dsadss did it!" />
     </div>
   </header>
 
